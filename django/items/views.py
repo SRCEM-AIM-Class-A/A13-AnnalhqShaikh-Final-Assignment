11b04c8e7cb6b4cb9bd686f6b61e22b30d50ec32
annalhq <155549764+annalhq@users.noreply.github.com>
2025-04-19 22:07:18 +0530

    feat: django database

---
+++ django/items/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import ItemForm
+
+def home(request):
+    items = Item.objects.all()
+
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ItemForm()
+
+    return render(request, 'home.html', {'items': items, 'form': form})
